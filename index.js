3f71b3da9c70c97c2c00785bc4374e762e7dd98f
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-05-20 13:46:34 +0200

    basic functionality done. looking into adding certain features that were not required

---
+++ index.js
@@ -0,0 +1,44 @@
+const colorPickerForm = document.getElementById("color-picker-form");
+const modal = document.getElementById("modal");
+const modalAlert = document.getElementById("modal-alert");
+
+function renderColors(colorsArr) {
+    let tempString = "";
+    colorsArr.forEach(color => {
+        tempString += `
+            <div class="color-container">
+                <div id="${color.hex.clean}" class="color pointer"></div>
+                <div class="color-footer">
+                    <p class="small">${color.hex.value}</p>
+                </div>
+            </div>
+        `;
+    });
+    document.getElementById("color-scheme").innerHTML = tempString;
+    colorsArr.forEach(color => {
+        document.getElementById(color.hex.clean).style.backgroundColor = color.hex.value;
+    });
+}
+
+colorPickerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const colorInputWithHashtag = document.getElementById("color-input").value;
+    const colorSchemeMode = document.getElementById("color-scheme-mode").value;
+    const colorInputWithoutHashtag = colorInputWithHashtag.substring(1);
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorInputWithoutHashtag}&mode=${colorSchemeMode}&count=5`)
+        .then(res => res.json())
+        .then(scheme => renderColors(scheme.colors));
+});
+
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("color")) {
+        navigator.clipboard.writeText(`#${e.target.id}`);
+        modal.style.display = "flex";
+        modalAlert.textContent = `Hex value: #${e.target.id} copied to clipboard.`;
+    }
+    if (e.target.id === "close-modal-btn") {
+        modal.style.display = "none";
+    }
+});
+
